c37d06185c98c85c294a6fcc1a6097234dab911f
G0DZ <0apm.tv@gmail.com>
2015-10-26 23:34:18 +0300

    Почти готово, осталось 4 задания

--- JavaTasks/src/Tasks.java
+++ JavaTasks/src/Tasks.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -437,8 +436,7 @@ public class Tasks {
 		}
         System.out.println("Завершение работы программы.");
 	}
-	
-	
+		
 	//public static void main(String args[]){
 	void task14(String args[]){
 		final Random random = new Random();
@@ -476,8 +474,8 @@ public class Tasks {
 		System.out.println("Завершение работы программы.");
 		}
 
-	public static void main(String args[]){
-	//void task15(String args[]){
+	//public static void main(String args[]){
+	void task15(String args[]){
 		final Random random = new Random();
 		int n = Integer.parseInt(args[0]); 	// считаем, что агрумент размера - верен.
 											//а почему бы нет?
@@ -508,10 +506,526 @@ public class Tasks {
 					
 			}
 		}
-		if(sumBetweenPos == 0)System.out.println("\n  Сумма элементов, расположенных между первым и \n"
+		System.out.println("\n  Сумма элементов, расположенных между первым и \n"
 				+ "вторым положительными элементами каждой строки: "+sumBetweenPos);
-		else
-			System.out.println("Нет элементов, удовлетворяющих условию.");
 		System.out.println("Завершение работы программы.");
 		}
+	
+	public static void main(String args[]){
+	//UNCOMPLETE
+	//void task16(String args[]){
+		final Random random = new Random();
+		int n = Integer.parseInt(args[0]); 	// считаем, что агрумент размера - верен.
+											//а почему бы нет?
+		int[][] matrix = new int[n][n];
+		System.out.println("Исходная матрица: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				matrix[i][j] = random.nextInt(2*n)-n;
+				System.out.printf("%3s", matrix[i][j]);
+			}
+			System.out.println();
+		}
+		//логика
+		int tempElement = 0;
+		for(int i = 0; i < (n+1)/2; i++){ 
+			for (int j = i; j < n-i; j++){
+				tempElement = matrix[i][j];
+				matrix[i][j] = matrix[i][n-j];
+				matrix[i][n-j] = matrix[n-i][n-j];
+				matrix[n-i][n-j] = matrix[n-i][j];
+				matrix[i][n-j] = tempElement;
+		  }
+		}
+		System.out.println("\nМатрица результата: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+				System.out.printf("%3s", matrix[i][j]);
+			System.out.println();
+		}
+        System.out.println("Завершение работы программы.");
+	}
+	
+	
+	//public static void main(String args[]){
+	//void task17()
+	//{
+	//	
+	//}
+	
+	//public static void main(String args[]){
+	void task18(String args[]){
+		final Random random = new Random();
+		int n = Integer.parseInt(args[0]); 	// считаем, что агрумент размера - верен.
+											//а почему бы нет?
+		int[][] matrix = new int[n][n];
+		System.out.println("Исходная матрица: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				matrix[i][j] = random.nextInt(2*n)-n;
+				System.out.printf("%3s", matrix[i][j]);
+			}
+			System.out.println();
+		}
+		//логика
+		int lMax = Integer.MIN_VALUE;
+		ArrayList<Integer> listRow = new ArrayList<Integer>(n);
+		ArrayList<Integer> listColumn = new ArrayList<Integer>(n);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(matrix[i][j] > lMax){
+					listRow.clear();
+					listColumn.clear();
+					listRow.add(i);
+					listColumn.add(j);
+					lMax = matrix[i][j];
+				}
+				if(matrix[i][j] == lMax){
+					if(!listRow.contains(i))
+						listRow.add(i);
+					if(!listColumn.contains(j))
+						listColumn.add(j);
+				}
+			}
+		}
+		System.out.println("\nМаксимальный элемент: "+lMax);
+		System.out.print("Строки, его содержащие:");
+		for(int i = 0; i < listRow.size(); i++)
+			System.out.print(listRow.get(i)+" ");
+		System.out.println();
+		System.out.print("Столбцы, его содержащие:");
+		for(int i = 0; i < listColumn.size(); i++)
+			System.out.print(listColumn.get(i)+" ");
+		System.out.println();
+		int[][] result = new int[n][n];
+		System.out.println("\nМатрица результата: ");
+		int k = 0; 
+		int m = 0;
+		for(int i = 0; i < n; i++){
+			if(!listRow.contains(i)){
+				m = 0;
+				for(int j = 0; j < n; j++){
+					if(!listColumn.contains(j)){
+						result[k][m] = matrix[i][j];
+						m++;
+					}
+				}
+			k++;
+			}
+		}
+		for(int i = 0; i < n-listRow.size(); i++){
+			for(int j = 0; j < n-listColumn.size(); j++)
+				System.out.printf("%3s", result[i][j]);
+			System.out.println();
+		}
+        System.out.println("Завершение работы программы.");
+	}
+
+	//public static void main(String args[]){
+	void task19(String args[]){
+		final Random random = new Random();
+		int n = Integer.parseInt(args[0]); 	// считаем, что агрумент размера - верен.
+											//а почему бы нет?
+		int[][] matrix = new int[n][n];
+		System.out.println("Исходная матрица: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				matrix[i][j] = random.nextInt(2*n)-n;
+				System.out.printf("%3s", matrix[i][j]);
+			}
+			System.out.println();
+		}
+		//логика
+		//времени мало, меняем 18 таск
+		ArrayList<Integer> listRow = new ArrayList<Integer>(n);
+		ArrayList<Integer> listColumn = new ArrayList<Integer>(n);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(matrix[i][j] == 0){
+					if(!listRow.contains(i))
+						listRow.add(i);
+					if(!listColumn.contains(j))
+						listColumn.add(j);
+				}
+			}
+		}
+		System.out.print("\nСтроки, содержащие 0: ");
+		for(int i = 0; i < listRow.size(); i++)
+			System.out.print(listRow.get(i)+" ");
+		System.out.print("\nСтолбцы, содержащие 0: ");
+		for(int i = 0; i < listColumn.size(); i++)
+			System.out.print(listColumn.get(i)+" ");
+		if(n-listRow.size() == 0 || n-listColumn.size() == 0){
+			System.out.println("Размерность матрицы - результата равна 0");
+		}
+		else{
+			int[][] result = new int[n-listRow.size()][n-listColumn.size()];
+			System.out.println("\n\nМатрица результата: ");
+			int k = 0; 
+			int m = 0;
+			for(int i = 0; i < n; i++){
+				if(!listRow.contains(i)){
+					m = 0;
+					for(int j = 0; j < n; j++){
+						if(!listColumn.contains(j)){
+							result[k][m] = matrix[i][j];
+							m++;
+						}
+					}
+				k++;
+				}
+			}
+			for(int i = 0; i < n-listRow.size(); i++){
+				for(int j = 0; j < n-listColumn.size(); j++)
+					System.out.printf("%3s", result[i][j]);
+				System.out.println();
+			}
+		}
+        System.out.println("Завершение работы программы.");
+	}
+	
+	//public static void main(String args[]){
+	//void task20(String args[]){
+	//	
+	//}
+	
+	//public static void main(String args[]){
+	void task21(String args[]){
+		final Random random = new Random();
+		int n = Integer.parseInt(args[0]); 	// считаем, что агрумент размера - верен.
+											//а почему бы нет?
+		int[][] matrix = new int[n][n];
+		System.out.println("Исходная матрица: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				matrix[i][j] = random.nextInt(2*n)-n;
+				System.out.printf("%3s", matrix[i][j]);
+			}
+			System.out.println();
+		}
+		//логика
+		int k = 0;
+		int temp = 0;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+			{
+				if(matrix[i][j] == 0){
+					k = n-1; //последний элемент строки
+					if(matrix[i][k] != 0){
+						//обмен
+						for(int ex = j; ex < k; ex++){
+							temp = matrix[i][ex];
+							matrix[i][ex] = matrix[i][ex+1];
+							matrix[i][ex+1] = temp;
+						}
+					}
+					else
+						k--;
+				}
+			}	
+		}
+		System.out.println("\nМатрица результата: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+				System.out.printf("%3s", matrix[i][j]);
+			System.out.println();
+		}
+        System.out.println("Завершение работы программы.");
+	}
+	
+	//public static void main(String args[]){
+	void task22(String args[]){
+		final Random random = new Random();
+		int n = Integer.parseInt(args[0]); 	// считаем, что агрумент размера - верен.
+											//а почему бы нет?
+		//ладно, сгенерируем double
+		double[][] matrix = new double[n][n];
+		System.out.println("Исходная матрица: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				matrix[i][j] = -n + (2*n)*random.nextDouble();
+				System.out.printf("%6.2f", matrix[i][j]);
+			}
+			System.out.println();
+		}
+		//логика
+		int[][] result = new int[n][n];
+		System.out.println("\nМатрица результата: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				result[i][j] = (int)Math.round(matrix[i][j]);
+				System.out.printf("%3s", result[i][j]);
+			}
+			System.out.println();
+		}
+        System.out.println("Завершение работы программы.");
+	}
+	
+	//public static void main(String args[]){
+	void task23(String args[]){
+		final Random random = new Random();
+		int n = Integer.parseInt(args[0]); 	// считаем, что агрумент размера - верен.
+											//а почему бы нет?
+		int[][] matrix = new int[n][n];
+		System.out.println("Исходная матрица: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				matrix[i][j] = random.nextInt(2*n)-n;
+				System.out.printf("%3s", matrix[i][j]);
+			}
+			System.out.println();
+		}
+		//логика
+		//тут, собственно, фича: минимум/максимум - первый такой элемент в сроке
+		int[] minRows = new int[n]; //величины всех минимумов, построчно с первой
+		int[] maxColumns = new int[n]; //величины всех минимумов, по столбцам с первого
+		int minMax = 0; 		//для организации проверки
+		//построчные минимумы
+		for (int i = 0; i < n; i++){
+		    minMax = matrix[i][0];
+		    for (int j = 1; j < n; j++)
+		        if (matrix[i][j] < minMax){ 
+		        	minMax= matrix[i][j]; 
+		        	}
+		    minRows[i] = minMax;    
+		}
+		//максимумы в столбцах 
+		for (int j = 0; j < n; j++){
+		    minMax = matrix[0][j];
+		    for (int i = 1; i < n; i++)
+		        if (matrix[i][j] > minMax){ 
+		        	minMax= matrix[i][j]; 
+		        	}
+		    maxColumns[j] = minMax;    
+		}
+		int count = 0; //число седловых точек
+		for (int i = 0; i < n; i++){
+		    for (int j = 0; j < n; j++ ){
+		        if (matrix[i][j] == minRows[i] && matrix[i][j] == maxColumns[j]){
+		            System.out.printf("\n[%s][%s]=%s", i, j, matrix[i][j]);
+		            count++;
+		        }
+		    }
+		}
+		System.out.println("\nЧисло седловых точек: "+count);
+        System.out.println("Завершение работы программы.");
+	}
+	
+	//public static void main(String args[]){
+	void task24(String args[]){
+		final Random random = new Random();
+		int n = Integer.parseInt(args[0]); 	// считаем, что агрумент размера - верен.
+											//а почему бы нет?
+		int[][] matrix = new int[n][n];
+		System.out.println("Исходная матрица: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				matrix[i][j] = random.nextInt(2*n)-n;
+				System.out.printf("%3s", matrix[i][j]);
+			}
+			System.out.println();
+		}
+		//логика
+		//вспоминаем первый таск, например
+		int[] summary = new int[n];
+		for(int i = 0; i < n; i++){
+			summary[i] = 0;
+			for(int j = 0; j < n; j++){
+				summary[i]+=matrix[i][j];
+			}
+		}
+		for(int i = 1; i < n; i++){
+            int currString[] = matrix[i].clone(); //впервые используем clone для матрицы
+            int previos = i - 1;
+                while(previos >= 0 && summary[previos] > summary[i]){
+                	matrix[previos+1] = matrix[previos]; //двигаемся
+                	matrix[previos] = currString.clone();
+                    previos--;
+                }
+        }
+		System.out.println("\nМатрица результата: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				System.out.printf("%3s", matrix[i][j]);
+			}
+			System.out.println();
+		}
+        System.out.println("Завершение работы программы.");
+	}
+	
+	//public static void main(String args[]){
+	void task25(String args[]){
+		final Random random = new Random();
+		int n = Integer.parseInt(args[0]); 	// считаем, что агрумент размера - верен.
+											//а почему бы нет?
+		//исплючаем ошибки размерности
+		if(n == Integer.MAX_VALUE){
+			System.out.println("Некорректное значение размера, выход");
+			return;
+		}
+		int[][] matrix = new int[n][n];
+		System.out.println("Исходная матрица: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				matrix[i][j] = random.nextInt(2*n)-n;
+				System.out.printf("%3s", matrix[i][j]);
+			}
+			System.out.println();
+		}
+		//логика
+		//допустим, впишем массив в рамку
+		int[][] result = new int[n+2][n+2];
+		for(int i = 0; i < n+2; i++){
+			for(int j = 0; j < n+2; j++){
+				if(i == 0 || i == n+1 || j == 0 || j == n+1)
+					result[i][j] = Integer.MAX_VALUE;
+				else
+					result[i][j] = matrix[i-1][j-1];
+			}
+		}
+		for(int i = 0; i < n+2; i++){
+			for(int j = 0; j < n+2; j++){
+				System.out.printf("%3s", result[i][j]);
+			}
+			System.out.println();
+		}
+		int count = 0; //число лок. мин
+		for(int i = 1; i < n+1; i++){
+			for(int j = 1; j < n+1; j++){
+				if(result[i][j] < result[i-1][j-1] &&
+						result[i][j] < result[i][j-1] &&
+						result[i][j] < result[i-1][j] &&
+						result[i][j] < result[i+1][j] &&
+						result[i][j] < result[i][j+1] &&
+						result[i][j] < result[i+1][j+1] &&
+						result[i][j] < result[i+1][j-1] &&
+						result[i][j] < result[i-1][j+1]){
+					count++;
+					System.out.printf("Найден локальный минимум в координатах "
+							+ "[%s][%s]=%s", i-1, j-1, result[i][j]+"\n");
+				}
+			}
+		}
+		System.out.println("Число локальных минимумов: "+count);
+        System.out.println("Завершение работы программы.");
+	}
+		
+	//public static void main(String args[]){
+	void task26(String args[]){
+		final Random random = new Random();
+		int n = Integer.parseInt(args[0]); 	// считаем, что агрумент размера - верен.
+											//а почему бы нет?
+		//исплючаем ошибки размерности
+		if(n == Integer.MAX_VALUE){
+			System.out.println("Некорректное значение размера, выход");
+			return;
+		}
+		int[][] matrix = new int[n][n];
+		System.out.println("Исходная матрица: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				matrix[i][j] = random.nextInt(2*n)-n;
+				System.out.printf("%3s", matrix[i][j]);
+			}
+			System.out.println();
+		}
+		//логика
+		//допустим, впишем массив в рамку
+		int[][] result = new int[n+2][n+2];
+		for(int i = 0; i < n+2; i++){
+			for(int j = 0; j < n+2; j++){
+				if(i == 0 || i == n+1 || j == 0 || j == n+1)
+					result[i][j] = Integer.MAX_VALUE;
+				else
+					result[i][j] = matrix[i-1][j-1];
+			}
+		}
+		for(int i = 0; i < n+2; i++){
+			for(int j = 0; j < n+2; j++){
+				System.out.printf("%3s", result[i][j]);
+			}
+			System.out.println();
+		}
+		int count = 0; //число лок. мин
+		ArrayList<Integer> list = new ArrayList<Integer>(); 
+		for(int i = 1; i < n+1; i++){
+			for(int j = 1; j < n+1; j++){
+				if(result[i][j] > result[i-1][j-1] &&
+						result[i][j] > result[i][j-1] &&
+						result[i][j] > result[i-1][j] &&
+						result[i][j] > result[i+1][j] &&
+						result[i][j] > result[i][j+1] &&
+						result[i][j] > result[i+1][j+1] &&
+						result[i][j] > result[i+1][j-1] &&
+						result[i][j] > result[i-1][j+1]){
+					count++;
+					list.add(result[i][j]);
+					System.out.printf("Найден локальный максимум в координатах "
+							+ "[%s][%s]=%s", i, j, result[i][j]+"\n");
+				}
+			}
+		}
+		int lMax = Integer.MIN_VALUE;
+		for(int i = 0; i < list.size(); i++){
+			if(list.get(i) > lMax)
+				lMax = list.get(i);
+		}
+		System.out.println("Число локальных максимумов: "+count);
+		if(list.size() != 0)
+			System.out.println("Наибольший из них: "+lMax);
+        System.out.println("Завершение работы программы.");
+	}
+
+	//public static void main(String args[]){
+	void task27(String args[]){
+		final Random random = new Random();
+		int n = Integer.parseInt(args[0]); 	// считаем, что агрумент размера - верен.
+											//а почему бы нет?
+		int[][] matrix = new int[n][n];
+		System.out.println("Исходная матрица: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				matrix[i][j] = random.nextInt(2*n)-n;
+				System.out.printf("%3s", matrix[i][j]);
+			}
+			System.out.println();
+		}
+		//логика
+		//меняем 24 таск
+		int[] summary = new int[n];
+		int result[][] = new int[n][n]; //новая матрица столбцы = строки
+		for(int j = 0; j < n; j++){
+			summary[j] = 0;
+			for(int i = 0; i < n; i++){
+				summary[j]+=Math.abs(matrix[i][j]);
+				result[j][i] = matrix[i][j];
+			}
+		}
+		for(int i = 0; i < n; i++){
+			System.out.printf("%3s", summary[i]);
+		}
+		for(int i = 1; i < n; i++){
+            int currString[] = result[i].clone(); //впервые используем clone для матрицы
+            int previos = i - 1;
+                while(previos >= 0 && summary[previos] > summary[i]){
+                	result[previos+1] = result[previos]; //двигаемся
+                	result[previos] = currString.clone();
+                    previos--;
+                }
+        }
+		//столбцы = строки, меняем обратно
+		for(int j = 0; j < n; j++){
+			for(int i = 0; i < n; i++){
+				matrix[j][i] = result[i][j];
+			}
+		}
+		System.out.println("\nМатрица результата: ");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				System.out.printf("%3s", matrix[i][j]);
+			}
+			System.out.println();
+		}
+        System.out.println("Завершение работы программы.");
+	}
 }
